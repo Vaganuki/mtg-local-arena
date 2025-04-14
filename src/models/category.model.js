ecfee6fdba2ebe6b9bd4b52c477db00020e9d37d
Vaganuki <hugo.warnotte@gmail.com>
2025-04-14 14:25:45 +0200

    category model

---
+++ src/models/category.model.js
@@ -0,0 +1,24 @@
+const {DataTypes} = require("sequelize");
+const categoryModel = (Sequelize) => {
+    return Sequelize.define(
+        'category',
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+                allowNull: false
+            },
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false
+            }
+        },
+        {
+            tableName: 'categories',
+            timestamps: false,
+        }
+    );
+};
+
+module.exports = categoryModel;
