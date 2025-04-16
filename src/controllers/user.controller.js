2bd88933ac4944ae2864708405cc2e59b1db7a7c
Vaganuki <hugo.warnotte@gmail.com>
2025-04-16 09:59:49 +0200

    userController login

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,9 @@
 const db = require("../models");
 const argon2 = require("argon2");
 const userController = {
-
     addNewUser: async (req, res) => {
         try {
-            const { username, email, password, firstname, lastname, birthdate } = req.body;
+            const {username, email, password, firstname, lastname, birthdate} = req.body;
             const user = await db.users.findOne({
                 where: {
                     username,
@@ -21,7 +20,7 @@ const userController = {
                     lastname,
                     birthdate,
                 });
-                return res.status(200).json(data);
+                return res.status(201).json(data);
             } else {
                 return res.status(400).json({error: "Username or email already in use"});
             }
@@ -29,8 +28,42 @@ const userController = {
             console.error(err);
             res.status(500).send({error: err})
         }
-    }
-
+    },
+    logUser: async (req, res) => {
+        let user = null;
+        const {mail, password} = req.body;
+        try {
+            user = await db.users.findOne({
+                where: mail
+            });
+        } catch (err) {
+            console.error(err);
+            res.status(500).send({error: err});
+            return;
+        }
+        if (user) {
+            try {
+                if (await argon2.verify(password, user.password)) {
+                    const token = jwt.sign({
+                        id: user.id,
+                        username: user.username,
+                        email: user.email,
+                    }, process.env.JWT_SECRET, {
+                        expiresIn: process.env.JWT_EXPIRE,
+                        algorithm: "HS256",
+                    });
+                    res.status(202).json({token});
+                } else {
+                    res.status(401).send({error: "Invalid Credentials"});
+                }
+            } catch (err) {
+                console.error(err);
+                res.status(500).send({error: err});
+            }
+        } else {
+            res.status(401).send({error: "Invalid Credentials"});
+        }
+    },
 };
 
 module.exports = userController;
