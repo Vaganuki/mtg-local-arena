3b5f7e5b4f3f050df2d2ee2fb31efc783130800b
Vaganuki <hugo.warnotte@gmail.com>
2025-04-16 09:49:54 +0200

    userController addNewUser

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,35 @@
+const db = require("../models");
+const userController = {
+
+    addNewUser: async (req, res) => {
+        try {
+            const { username, email, password, firstname, lastname, birthdate } = req.body;
+            const user = await db.users.findOne({
+                where: {
+                    username,
+                    email,
+                }
+            });
+            if (!user) {
+                const hash = password;
+                const data = await db.users.create({
+                    username,
+                    email,
+                    password: hash,
+                    firstname,
+                    lastname,
+                    birthdate,
+                });
+                return res.status(200).json(data);
+            } else {
+                return res.status(400).json({error: "Username or email already in use"});
+            }
+        } catch (err) {
+            console.error(err);
+            res.status(500).send({error: err})
+        }
+    }
+
+};
+
+module.exports = userController;
