f5018b96630eab649e356ffa58ca7de0d598df10
Vaganuki <hugo.warnotte@gmail.com>
2025-04-16 09:30:57 +0200

    eventController getAllEvents

--- src/controllers/event.controller.js
+++ src/controllers/event.controller.js
@@ -10,9 +10,9 @@ const eventController = {
             const events = await db.events.findAll({
                 where: {date: {[Op.gte]: new Date()}},
                 include: [
-                    db.inscriptions,
-                    db.formats,
                     db.categories,
+                    db.formats,
+                    db.inscriptions,
                 ],
                 limit: limit,
                 offset: offset,
@@ -24,7 +24,41 @@ const eventController = {
             } else {
                 res.status(404).send({error: 'Not Found'});
             }
+        } catch (err) {
+            console.error(err);
+            res.status(500).send({error: err});
+        }
+    },
+    getAllEvents: async (req, res) => {
+        try {
+            const {id_category, id_format, date, searchQuery} = req.query;
+            const filters = {};
 
+            if (id_category) {
+                filters.category_id = id_category;
+            }
+            if (id_format) {
+                filters.format = id_format;
+            }
+            if (date) {
+                filters.date = date;
+            }
+            if (searchQuery) {
+                filters[Op.or] = [
+                    {name: {[Op.iLike]: `%${searchQuery}%`}},
+                    {description: {[Op.iLike]: `%${searchQuery}%`}},
+                ]
+            }
+            const events = await db.events.findAll({
+                where: filters,
+                include: [
+                    db.categories,
+                    db.formats,
+                    db.inscriptions,
+                ],
+                order: [['date', 'ASC']],
+            });
+            return res.status(200).json(events);
         } catch (err) {
             console.error(err);
             res.status(500).send({error: err});
