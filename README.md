6733b8fe8e3f62bebcdaf3afbeeaf5150293193e
Vaganuki <hugo.warnotte@gmail.com>
2025-07-04 11:23:52 +0200

    Apparently we're doin TypeScript now

--- README.md
+++
@@ -1,2 +0,0 @@
-# mtg-local-arena
-An Api aimed for local play group to create some events.
--- src/core/utils.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-
-imageDelete = (path) => {
-    if (path != null) {
-        fs.unlinkSync(__dirname + '/../public/images/' + path);
-    }
-}
-
-module.exports = imageDelete;
